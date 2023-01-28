6ee5eff8705c943ea5b76254436a7ef8e4c2f4a4
Oscar Castillo <oscar_cm@outlook.com>
2023-01-27 20:36:32 -0600

    Add avatar label to user form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permited_parameter
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :avatar])
   end
 
 end
