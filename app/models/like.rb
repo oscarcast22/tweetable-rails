c8427c3ab843436012920a2310ba11cafe63e521
Oscar Castillo <oscar_cm@outlook.com>
2023-01-27 23:59:46 -0600

    Create seeds of users and tweets

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :tweet
+  belongs_to :user, dependent: :destroy
+  belongs_to :tweet, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,57 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+puts "Destroying all data"
+
+User.destroy_all
+Tweet.destroy_all
+
+puts "seeding data"
+
+puts "start creating admin user"
+
+new_admin = User.create(username: "admin", 
+                        name: "admin",
+                        email: "admin@mail.com",
+                        password: "supersecret",
+                        password_confirmation: "supersecret",
+                        role: "admin")
+  if new_admin.save
+    puts "Created admin user Succesfully"
+    new_admin.avatar.attach(io: File.open("public/images/avatar.jpg"), filename: "avatar.jpg")
+  else
+    puts "Errors: #{new_admin.errors.full_messages}"
+  end
+
+puts "start creating random members users"
+
+4.times do
+  new_user = User.create(username: Faker::Internet.unique.username, 
+                         name: Faker::FunnyName.unique.name,
+                         email: Faker::Internet.unique.email,
+                         password: "123456",
+                         password_confirmation: "123456",
+                         role: "member")
+  if new_user.save
+    puts "Created random users Succesfully"
+    new_user.avatar.attach(io: File.open("public/images/avatar.jpg"), filename: "avatar.jpg")
+  else
+    puts "Errors: #{new_user.errors.full_messages}"
+  end
+
+  puts "creating tweets"
+
+  3..6.times do
+    new_tweet = Tweet.create(
+      body:Faker::Quote.famous_last_words,
+      user_id: new_user.id
+    )
+    if new_tweet.save
+      puts "Created Tweets Succesfully"
+    else
+      puts "Errors: #{new_tweet.errors.full_messages}"
+    end
+  end
+end
