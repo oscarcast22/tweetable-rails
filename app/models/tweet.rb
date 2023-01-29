991c9795b29f6a4a4bb71a2a07759df209d5da16
Oscar Castillo <oscar_cm@outlook.com>
2023-01-29 00:30:37 -0600

    Add styles to new-session page

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,9 +1,11 @@
 class Tweet < ApplicationRecord
   validates :body, presence: true, length: { maximum: 140 }
+  has_many :likes, dependent: :destroy
   has_many :retweet, class_name: "Tweet",
                      foreign_key: "replied_to_id",
                      inverse_of: "replied_to",
-                     dependent: :destroy
+                     dependent: :destroy,
+                     counter_cache: :retweet
   
   belongs_to :replied_to, class_name: "Tweet", optional: true
 
